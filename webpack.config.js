f76ca7e904e38ecf6f07204a38a1cff06ffc3d17
toby <toby@canva.com>
2017-01-08 02:00:56 +1100

    flow

--- webpack.config.js
+++ webpack.config.js
@@ -11,10 +11,7 @@ module.exports = {
       {
         test: /\.jsx?$/,
         loader: 'babel',
-        exclude: /node_modules/,
-        query: {
-          presets: ['es2015', 'react']
-        }
+        exclude: /node_modules/
       }
     ]
   },

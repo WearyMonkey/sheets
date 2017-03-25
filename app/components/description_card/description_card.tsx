3bfdc20b2a2b8e62702cd2556dcbd791ef9a1775
toby <toby@canva.com>
2017-03-25 13:44:32 +1100

    upload

--- app/components/description_card/description_card.tsx
+++ app/components/description_card/description_card.tsx
@@ -13,27 +13,37 @@ import { FileUploader } from 'services/file_uploader';
 export class DescriptionCard extends React.Component<{description: Description}, { }> {
 
   fileUploader: FileUploader = new FileUploader();
+  @observable uploadPreview?: string|null;
+  @observable uploadPercentage: number = 0;
 
   render() {
     const { description } = this.props;
     let content;
     switch (description.type) {
       case 'IMAGE':
-        content = <img src={description.imageUrl || ''}/>;
+        const url = this.uploadPreview || description.imageUrl;
+        content = <img src={url || ''}/>;
         break;
       case 'TEXT':
         content = <TextEditor textState={description.textState} onChange={this.onEditorChange} />;
         break;
     }
-
     return (<DropZone onDrop={this.onImageDrop} accept="image/*" disableClick={true} multiple={false} className={styles.dropZone} activeClassName={styles.activeDropZone} >
       { content }
     </DropZone>);
   }
 
   @action
-  onImageDrop = ([file] : {}[]) => {
-
+  onImageDrop = ([file] : (File & {preview: string})[]) => {
+    this.props.description.type = 'IMAGE';
+    this.uploadPreview = file.preview;
+    this.uploadPercentage = 0;
+    this.fileUploader.upload(file, action((uploaded: number, total: number) => {
+      this.uploadPercentage = Math.floor(uploaded / total * 100);
+    })).then(action((url: string) => {
+      this.uploadPreview = null;
+      this.props.description.imageUrl = url;
+    }));
   };
 
   @action
--- app/services/file_uploader.ts
+++ app/services/file_uploader.ts
@@ -1,7 +1,21 @@
 export class FileUploader {
 
-  upload() {
+  upload(file: File, progress: (uploaded: number, total: number) => void) : Promise<string> {
+    const bucketUrl = 'https://s3-ap-southeast-2.amazonaws.com/sheets-uploads';
+    const key = file.name;
+    const imageUrl = `${bucketUrl}/${key}`;
+    const oReq = new XMLHttpRequest();
+    const formData = new FormData();
+    formData.append('key', key);
+    formData.append('file', file);
 
+    return new Promise((resolve, reject) => {
+      oReq.upload.onprogress = e => progress(e.loaded, e.total);
+      oReq.addEventListener("load", () => resolve(imageUrl));
+      oReq.addEventListener("error", reject);
+      oReq.addEventListener("abort", reject);
+      oReq.open("post", bucketUrl);
+      oReq.send(formData);
+    })
   }
-
 }
